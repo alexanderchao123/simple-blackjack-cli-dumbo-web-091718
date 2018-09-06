171e5492b13447c2af4d0d87f53e8041ffc60c9e
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-06 04:14:29 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,16 +55,6 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-# def runner
-#   welcome
-#   sum = initial_round
-#   until sum > 21
-#     sum = hit?(sum)
-#     display_card_total(sum)
-#   end
-#   end_game(sum)
-# end
-
 def runner
   welcome
   sum = initial_round
@@ -75,5 +65,3 @@ def runner
   end
   end_game(sum)
 end
-
-# runner
